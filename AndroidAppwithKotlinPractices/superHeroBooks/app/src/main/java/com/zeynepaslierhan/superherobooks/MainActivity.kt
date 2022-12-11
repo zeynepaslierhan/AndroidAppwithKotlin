98f7c9f05182a180bfa7dd2db8aa5c5f160a12b5
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-12-12 01:07:29 +0300

    Update MainActivity.kt

--- AndroidAppwithKotlinPractices/superHeroBooks/app/src/main/java/com/zeynepaslierhan/superherobooks/MainActivity.kt
+++ AndroidAppwithKotlinPractices/superHeroBooks/app/src/main/java/com/zeynepaslierhan/superherobooks/MainActivity.kt
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.recycle_row.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
