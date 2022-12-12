ff52bbaed7ca7ed54169d136491810143bfcf91c
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-12-13 01:31:50 +0300

    LogOut yapıldı, Menu eklendi

--- AndroidAppwithKotlinPractices/photoSharingwithFirebase/app/src/main/java/com/zeynepaslierhan/photosharing/FeedActivity.kt
+++ AndroidAppwithKotlinPractices/photoSharingwithFirebase/app/src/main/java/com/zeynepaslierhan/photosharing/FeedActivity.kt
@@ -1,11 +1,43 @@
 package com.zeynepaslierhan.photosharing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class FeedActivity : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+
+        auth=FirebaseAuth.getInstance()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater= menuInflater
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.sharePhoto){
+
+            val intent = Intent(this,SharePhotoActivity::class.java)
+            startActivity(intent)
+
+        }else if(item.itemId == R.id.LogOut){
+
+            auth.signOut()
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ AndroidAppwithKotlinPractices/photoSharingwithFirebase/app/src/main/java/com/zeynepaslierhan/photosharing/SharePhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.zeynepaslierhan.photosharing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharePhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_photo)
+    }
+}
